e19f8e0e144ea74bf0fe32c2bed3dc7b92f59414
ccw <ccw.280231@ca.ibm.com>
2023-12-05 11:36:57 -0500

    update poll

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -1,6 +1,13 @@
 node('linux')
 {
   stage ('Poll') {
+   // Poll from upstream:
+    checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: "https://github.com/IBM/zos-pstree.git"]]])
     checkout([
       $class: 'GitSCM',
       branches: [[name: '*/main']],
